0b0ee4d892e350e7c237dd085e9a85fc2733c6ee
Aaron Ye <aye@ambiq.com>
2023-12-25 09:37:10 +0800

    Update ble_firmware_update sample.

--- samples/bluetooth/ble_firmware_update/README.rst
+++ samples/bluetooth/ble_firmware_update/README.rst
@@ -6,8 +6,11 @@ ble_firmware_update
 Overview
 ********
 
-A sample that can be used to update the BLE controller firmware of
-Ambiq Apollo4x EVB with Bluetooth Low Engegy supported.
+A sample that mainly be used to update the BLE controller firmware
+of Ambiq Apollo4x soc with Bluetooth Low Engegy supported. The user
+can replace the header file in include directory and uncomment the
+corresponding macro in main.c for the update.
+
 
 Building and Running
 ********************
@@ -23,4 +26,23 @@ Sample Output
 
 .. code-block:: console
 
-    BLE firmware update application
+   le_firmware_update: BLE Firmware Update Application
+   bt_controller: BLE Controller Info:
+   bt_controller: 	FW Ver:      1.21.2.0
+   bt_controller: 	Chip ID0:    0x92492492
+   bt_controller: 	Chip ID1:    0x4190d413
+   bt_controller: No new image to upgrade
+   bt_controller: BLE Controller FW Auth Passed, Continue with FW
+   bt_apollox_driver: BT controller initialized
+   bt_hci_core: Identity: 22:89:67:45:23:22 (public)
+   bt_hci_core: HCI: version 5.1 (0x0a) revision 0x0115, manufacturer 0x09ac
+   bt_hci_core: LMP: version 5.1 (0x0a) subver 0x0115
+   ble_firmware_update: Reset BLE controller to do a forcing upgrade
+   bt_controller: BLE Controller Info:
+   bt_controller: 	FW Ver:      1.21.2.0
+   bt_controller: 	Chip ID0:    0x92492492
+   bt_controller: 	Chip ID1:    0x4190d413
+   bt_controller: BLE Controller Requires FW
+   bt_controller: BLE controller upgrade in progress, wait...
+   bt_controller: Update Done
+   ble_firmware_update: BLE Firmware Update Application Done!
--- samples/bluetooth/ble_firmware_update/src/main.c
+++ samples/bluetooth/ble_firmware_update/src/main.c
@@ -5,16 +5,25 @@
  */
 
 /**
- * @brief A sample to demostrate the BLE controller firmware update
+ * @brief A sample to demostrate the BLE controller firmware/patch update
  * in Ambiq Apollo4x EVB with Bluetooth Low Engery supported.
  */
 
+#include <zephyr/logging/log.h>
 #include <zephyr/drivers/bluetooth/hci_driver.h>
+
 #include "am_devices_cooper.h"
 
+#define LOG_MODULE_NAME ble_firmware_update
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+/* Uncomment one of UPDATE_BLE_FW, UPDATE_BLE_INFO0 and UPDATE_BLE_INFO1
+ * macros to enable the updating of the BLE controller firmware, controller
+ * info0 or info1 patch.
+ */
 #define UPDATE_BLE_FW
-// #define UPDATE_BLE_INFO0
-// #define UPDATE_BLE_INFO1
+/* #define UPDATE_BLE_INFO0 */
+/* #define UPDATE_BLE_INFO1 */
 #if defined(UPDATE_BLE_FW)
 #include "ble_fw_image.h"
 #define image_bin       ble_fw_image_bin
@@ -37,20 +46,15 @@
 #warning No update image available
 #endif
 
-//*****************************************************************************
-//
-// Global Variables
-//
-//*****************************************************************************
 static am_devices_cooper_sbl_update_data_t g_sUpdateImage = {(uint8_t *)&image_bin,
 							     sizeof(image_bin), image_type, 0};
 
 static int set_update_sign(uint32_t ui32Sign)
 {
 	struct net_buf *buf;
+	uint8_t *p;
 	uint8_t sign_cmd[4] = {UINT32_TO_BYTE0(ui32Sign), UINT32_TO_BYTE1(ui32Sign),
 			       UINT32_TO_BYTE2(ui32Sign), UINT32_TO_BYTE3(ui32Sign)};
-	uint8_t *p;
 
 	buf = bt_hci_cmd_create(HCI_VSC_UPDATE_FW_CFG_CMD_OPCODE, HCI_VSC_UPDATE_FW_CFG_CMD_LENGTH);
 	if (!buf) {
@@ -65,27 +69,27 @@ static int set_update_sign(uint32_t ui32Sign)
 int main()
 {
 	int err;
-	printk("BLE Firmware Update Application\n");
+	LOG_INF("BLE Firmware Update Application");
 	ble_get_binary(&g_sUpdateImage);
 	err = bt_enable(NULL);
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		LOG_ERR("Bluetooth init failed (err %d)", err);
 		return err;
 	}
 
 	err = set_update_sign(BLE_UPDATE_SIGN);
 	if (err) {
-		printk("Set signaure failed (err %d)\n", err);
+		LOG_ERR("Set signaure failed (err %d)\n", err);
 		return err;
 	}
 
-	printk("*****Reset BLE controller to do a forcing upgrade*****\n");
+	LOG_INF("Reset BLE controller to do a forcing upgrade");
 	err = am_devices_cooper_reset_with_sbl_check();
 	if (err) {
-		printk("BLE Firmware Update Failed (err %d)\n", err);
+		LOG_ERR("BLE Firmware Update Failed (err %d)", err);
 		return err;
 	} else {
-		printk("BLE Firmware Update Application Done!\r\n");
+		LOG_INF("BLE Firmware Update Application Done!");
 	}
 
 	return 0;
