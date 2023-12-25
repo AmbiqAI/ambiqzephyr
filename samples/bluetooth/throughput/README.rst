cb6b3d334cc75654a07ef037c11fe2249571c103
Aaron Ye <aye@ambiq.com>
2023-12-25 09:37:09 +0800

    Create Ambiq throughput service and example.

---
+++ samples/bluetooth/throughput/README.rst
@@ -0,0 +1,22 @@
+.. _throughput:
+
+Bluetooth: Throughput
+#####################
+
+Overview
+********
+
+A sample to demostrate the Ambiq BLE throughput service.
+
+
+Requirements
+************
+
+* A apollo4p_blue_kxr_evb
+* A phone with ambiq_ble_tester APP
+
+Building and Running
+********************
+
+
+See :ref:`bluetooth samples section <bluetooth-samples>` for details.
---
+++ samples/bluetooth/throughput/src/main.c
@@ -0,0 +1,101 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2023 Ambiq Micro Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/services/throughput.h>
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+	} else {
+		printk("Connected\n");
+		bt_throughput_conn_init(conn);
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_ready(void)
+{
+	int err;
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static struct bt_conn_auth_cb auth_cb_display = {
+	.cancel = auth_cancel,
+};
+
+static void throughput_notify(void)
+{
+	bt_throughput_notify();
+}
+
+int main(void)
+{
+	int err;
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	bt_ready();
+
+	bt_conn_auth_cb_register(&auth_cb_display);
+
+	while (1) {
+		k_sleep(K_USEC(1));
+
+		throughput_notify();
+	}
+	return 0;
+}
