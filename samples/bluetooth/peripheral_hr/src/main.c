661d92493e80707b70c6fdadc919bb5a378615e0
Aaron Ye <aye@ambiq.com>
2023-12-27 09:33:04 +0800

    Do low power initialization for power profiling.

--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -22,6 +22,8 @@
 #include <zephyr/bluetooth/services/bas.h>
 #include <zephyr/bluetooth/services/hrs.h>
 
+#include "am_mcu_apollo.h"
+
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
@@ -103,6 +105,51 @@ static void hrs_notify(void)
 	bt_hrs_notify(heartrate);
 }
 
+static int lp_init(void)
+{
+	am_hal_pwrctrl_mcu_memory_config_t McuMemCfg =
+	{
+		.eCacheCfg    = AM_HAL_PWRCTRL_CACHE_ALL,
+		.bRetainCache = true,
+		.eDTCMCfg     = AM_HAL_PWRCTRL_DTCM_128K,
+		.eRetainDTCM  = AM_HAL_PWRCTRL_DTCM_128K,
+		.bEnableNVM0  = true,
+		.bRetainNVM0  = false
+	};
+
+	am_hal_pwrctrl_sram_memcfg_t SRAMMemCfg =
+	{
+		.eSRAMCfg           = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithMCU     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithGFX     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithDISP    = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithDSP     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eSRAMRetain        = AM_HAL_PWRCTRL_SRAM_NONE
+	};
+
+    am_hal_pwrctrl_dsp_memory_config_t    DSPMemCfg =
+    {
+        .bEnableICache      = false,
+        .bRetainCache       = false,
+        .bEnableRAM         = false,
+        .bActiveRAM         = false,
+        .bRetainRAM         = false
+    };
+
+	am_hal_sysctrl_fpu_disable();
+    am_hal_pwrctrl_low_power_init();
+    am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_SIMOBUCK_INIT, 0);
+    am_hal_rtc_osc_disable();
+	am_hal_pwrctrl_periph_disable(AM_HAL_PWRCTRL_PERIPH_CRYPTO);
+	am_hal_pwrctrl_mcu_memory_config(&McuMemCfg);
+	am_hal_pwrctrl_sram_config(&SRAMMemCfg);
+	am_hal_pwrctrl_dsp_memory_config(AM_HAL_DSP0, &DSPMemCfg);
+	
+	return 0;
+}
+
+SYS_INIT(lp_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+
 int main(void)
 {
 	int err;
@@ -128,6 +175,8 @@ int main(void)
 
 		/* Battery level simulation */
 		bas_notify();
+
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
 	}
 	return 0;
 }
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,64 @@
  */
 
 #include <stdio.h>
+#include "am_mcu_apollo.h"
+
+static int lp_init(void)
+{
+	am_hal_pwrctrl_mcu_memory_config_t McuMemCfg =
+	{
+		.eCacheCfg    = AM_HAL_PWRCTRL_CACHE_ALL,
+		.bRetainCache = true,
+		.eDTCMCfg     = AM_HAL_PWRCTRL_DTCM_128K,
+		.eRetainDTCM  = AM_HAL_PWRCTRL_DTCM_128K,
+		.bEnableNVM0  = true,
+		.bRetainNVM0  = false
+	};
+
+	am_hal_pwrctrl_sram_memcfg_t SRAMMemCfg =
+	{
+		.eSRAMCfg           = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithMCU     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithGFX     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithDISP    = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eActiveWithDSP     = AM_HAL_PWRCTRL_SRAM_NONE,
+		.eSRAMRetain        = AM_HAL_PWRCTRL_SRAM_NONE
+	};
+
+    am_hal_pwrctrl_dsp_memory_config_t    DSPMemCfg =
+    {
+        .bEnableICache      = false,
+        .bRetainCache       = false,
+        .bEnableRAM         = false,
+        .bActiveRAM         = false,
+        .bRetainRAM         = false
+    };
+
+	am_hal_sysctrl_fpu_disable();
+    am_hal_pwrctrl_low_power_init();
+    am_hal_pwrctrl_control(AM_HAL_PWRCTRL_CONTROL_SIMOBUCK_INIT, 0);
+    am_hal_rtc_osc_disable();
+
+	am_hal_pwrctrl_periph_disable(AM_HAL_PWRCTRL_PERIPH_CRYPTO);
+	am_hal_pwrctrl_mcu_memory_config(&McuMemCfg);
+	am_hal_pwrctrl_sram_config(&SRAMMemCfg);
+	am_hal_pwrctrl_dsp_memory_config(AM_HAL_DSP0, &DSPMemCfg);
+    am_hal_gpio_state_write(55, AM_HAL_GPIO_OUTPUT_CLEAR);
+    am_hal_gpio_pinconfig(55, am_hal_gpio_pincfg_output);
+    am_hal_gpio_state_write(55, AM_HAL_GPIO_OUTPUT_SET);
+    am_hal_gpio_state_write(55, AM_HAL_GPIO_OUTPUT_CLEAR);
+
+	return 0;
+}
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD);
+
+	lp_init();
+
+	while (1) {
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
+	}
 	return 0;
 }
